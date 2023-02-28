8369404b3f094dd5492834d5e992f2459eacca4c
EddieTheEd <edwardleehuynh@gmail.com>
2023-02-28 10:15:28 +0800

    please

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,5 +1,5 @@
 const userPref = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'light' : 'dark'
-const currentTheme = localStorage.getItem('theme') ?? userPref
+const currentTheme = localStorage.getItem('theme') ?? 'dark'
 const syntaxTheme = document.querySelector("#theme-link");
 
 
@@ -18,8 +18,8 @@ const switchTheme = (e) => {
     syntaxTheme.href = '{{ $darkSyntax.Permalink }}';
   }
   else {
-    document.documentElement.setAttribute('saved-theme', 'dark')
-    localStorage.setItem('theme', 'dark')
+    document.documentElement.setAttribute('saved-theme', 'light')
+    localStorage.setItem('theme', 'light')
     syntaxTheme.href = '{{ $lightSyntax.Permalink }}';
   }
 }
